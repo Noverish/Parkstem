da804c62d295892beaa486d1f82f4113cbd0382a
Noverish <embrapers263@gmail.com>
2016-07-04 15:49:00 +0900

    activity를 만듬

---
+++ app/src/main/java/com/trams/parkstem/activity/InputNewCardActivity.java
@@ -0,0 +1,18 @@
+package com.trams.parkstem.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.trams.parkstem.R;
+
+/**
+ * Created by Noverish on 2016-07-04.
+ */
+public class InputNewCardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_new_card);
+    }
+}
