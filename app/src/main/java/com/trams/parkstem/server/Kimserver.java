c6e53c600a6067f20ff029b99693142c48bd5e13
Noverish <embrapers263@gmail.com>
2016-07-06 15:13:36 +0900

    실제로 kim과 moon을 merge 후 테스트로 만든 java파일 지움

--- app/src/main/java/com/trams/parkstem/server/Kimserver.java
+++
@@ -1,7 +0,0 @@
-package com.trams.parkstem.server;
-
-/**
- * Created by Noverish on 2016-07-06.
- */
-public class Kimserver {
-}
--- app/src/main/java/com/trams/parkstem/server/testServer.java
+++
@@ -1,7 +0,0 @@
-package com.trams.parkstem.server;
-
-/**
- * Created by monc2 on 2016-07-06.
- */
-public class testServer {
-}
