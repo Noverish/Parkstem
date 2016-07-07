a1bb302a5a1eb0178b0768480df243ac6b9715bd
Noverish <embrapers263@gmail.com>
2016-07-07 10:27:30 +0900

    카드등록의 카드종류 라디오버튼, 모든 EditText 한 줄만 입력되게, 회원가입 약관 체크박스 생성

--- app/src/main/java/com/trams/parkstem/activity/AssignActivity.java
+++ app/src/main/java/com/trams/parkstem/activity/AssignActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.CheckBox;
 
 import com.trams.parkstem.R;
 
@@ -11,6 +12,8 @@ import com.trams.parkstem.R;
  * Created by Noverish on 2016-07-04.
  */
 public class AssignActivity extends AppCompatActivity {
+    private CheckBox checkBox;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,8 @@ public class AssignActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        checkBox = (CheckBox) findViewById(R.id.assign_agree);
+        //checkBox.isChecked(); 는 체크박스의 체크가 되어 있는지 아닌지를 반환함
     }
 }
--- app/src/main/java/com/trams/parkstem/activity/InputNewCardActivity.java
+++ app/src/main/java/com/trams/parkstem/activity/InputNewCardActivity.java
@@ -2,8 +2,13 @@ package com.trams.parkstem.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.trams.parkstem.R;
 
@@ -11,6 +16,12 @@ import com.trams.parkstem.R;
  * Created by Noverish on 2016-07-04.
  */
 public class InputNewCardActivity extends AppCompatActivity {
+
+    private final int CARD_TYPE_PERSON = 0;
+    private final int CARD_TYPE_COMPANY = 1;
+    private int cardType;
+
+    private final String TAG = getClass().getSimpleName();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +29,43 @@ public class InputNewCardActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        LinearLayout cardTypeCompanyRadioButton = (LinearLayout) findViewById(R.id.activity_input_new_card_radio_company);
+        cardTypeCompanyRadioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                radioButtonOnClick((LinearLayout) v);
+            }
+        });
+        LinearLayout cardTypePersonRadioButton = (LinearLayout) findViewById(R.id.activity_input_new_card_radio_person);
+        cardTypePersonRadioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                radioButtonOnClick((LinearLayout) v);
+            }
+        });
+        cardType = CARD_TYPE_PERSON;
+    }
+
+    private void radioButtonOnClick(LinearLayout view) {
+        ImageView companyImage = (ImageView) findViewById(R.id.activity_input_new_card_radio_company_image);
+        TextView companyText = (TextView) findViewById(R.id.activity_input_new_card_radio_company_text);
+        ImageView personImage = (ImageView) findViewById(R.id.activity_input_new_card_radio_person_image);
+        TextView peresonText = (TextView) findViewById(R.id.activity_input_new_card_radio_person_text);
+
+        if(view.getId() == R.id.activity_input_new_card_radio_company && cardType == CARD_TYPE_PERSON) {
+            cardType = CARD_TYPE_COMPANY;
+            companyImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.img_ground));
+            companyText.setTextColor(ContextCompat.getColor(this, R.color.black));
+            personImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.img_round));
+            peresonText.setTextColor(ContextCompat.getColor(this, R.color.gray));
+        }
+        if(view.getId() == R.id.activity_input_new_card_radio_person && cardType == CARD_TYPE_COMPANY){
+            cardType = CARD_TYPE_PERSON;
+            companyImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.img_round));
+            companyText.setTextColor(ContextCompat.getColor(this, R.color.gray));
+            personImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.img_ground));
+            peresonText.setTextColor(ContextCompat.getColor(this, R.color.black));
+        }
     }
 }
