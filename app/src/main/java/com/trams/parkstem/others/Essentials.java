a7a65c4452fdd84ef4cccf9af2af6271e62fee60
vkfwlsdhs <dbwogy1@naver.com>
2016-07-14 19:51:02 +0900

    mobile ticket list 추가 (except 상세보기 버튼)

--- app/src/main/java/com/trams/parkstem/others/Essentials.java
+++ app/src/main/java/com/trams/parkstem/others/Essentials.java
@@ -1,18 +1,10 @@
 package com.trams.parkstem.others;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
-
-import com.trams.parkstem.R;
-
 /**
  * Created by JaeHyo on 2016-07-13.
  */
 public class Essentials {
+
     public static String numberWithComma(int number) {
         return numberWithComma(String.valueOf(number));
     }
@@ -28,26 +20,17 @@ public class Essentials {
         return number;
     }
 
-    public static void makePopup(Context context, String title, String content){
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public static String numberWithZero(int number) {
+        return numberWithZero(String.valueOf(number));
+    }
 
-        //R.layout.dialog는 xml 파일명이고  R.id.popup은 보여줄 레이아웃 아이디
-        View layout = inflater.inflate(R.layout.popup_layout, null);
-        AlertDialog.Builder aDialog = new AlertDialog.Builder(context);
+    public static String numberWithZero(long number) {
+        return numberWithZero(String.valueOf(number));
+    }
 
-        aDialog.setTitle(title); //타이틀바 제목
-        aDialog.setView(layout); //dialog.xml 파일을 뷰로 셋팅
-        TextView contentTextView = (TextView) layout.findViewById(R.id.popup_layout_content);
-        contentTextView.setText(content);
-
-        //그냥 닫기버튼을 위한 부분
-        aDialog.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-            }
-        });
-
-        //팝업창 생성
-        AlertDialog ad = aDialog.create();
-        ad.show();//보여줌!
+    public static String numberWithZero(String number) {
+        if(number.length()==1)
+            number = "0" + number;
+        return number;
     }
 }
---
+++ app/src/main/java/com/trams/parkstem/view/TicketMobileView.java
@@ -0,0 +1,70 @@
+package com.trams.parkstem.view;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.trams.parkstem.R;
+import com.trams.parkstem.others.Essentials;
+import com.trams.parkstem.server.ServerClient;
+
+import java.util.Calendar;
+
+/**
+ * Created by JaeHyo on 2016-07-13.
+ */
+public class TicketMobileView extends LinearLayout {
+    public TicketMobileView(Context context, ServerClient.Ticket ticket) {
+        super(context);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        try{
+            inflater.inflate(R.layout.ticket_mobile_item, this);
+
+            ServerClient.ParkInfo parkInfo = ServerClient.getInstance().parkInfo(ticket.local_id);
+
+            TextView name = (TextView) findViewById(R.id.ticket_mobile_item_name);
+            name.setText(ticket.ticket_name);
+
+            name = (TextView) findViewById(R.id.ticket_mobile_item_place);
+            name.setText(parkInfo.local_content);
+
+            name = (TextView) findViewById(R.id.ticket_mobile_item_place_2);
+            name.setText(parkInfo.local_address);
+
+            Calendar ca = Calendar.getInstance();
+            String date = ca.get(Calendar.YEAR) + ".";
+
+            date += Essentials.numberWithZero(ca.get(Calendar.MONTH) + 1) + ".";
+
+            date += Essentials.numberWithZero(ca.get(Calendar.DAY_OF_MONTH));
+
+            name = (TextView) findViewById(R.id.ticket_mobile_item_date);
+            name.setText(date);
+
+            name = (TextView) findViewById(R.id.ticket_mobile_item_before_price);
+            name.setText("1시간 " + (char)0xffe6 + Essentials.numberWithComma(ticket.original_price) );
+            name.setPaintFlags(name.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+            name = (TextView) findViewById(R.id.ticket_mobile_item_after_price);
+            name.setText("1시간 " + (char)0xffe6 + Essentials.numberWithComma(ticket.price) );
+
+            ca.add(Calendar.DAY_OF_MONTH, 100);
+
+            date = "유효기간:" + date + "-" + ca.get(Calendar.YEAR) + ".";
+
+            date += Essentials.numberWithZero(ca.get(Calendar.MONTH) + 1) + ".";
+
+            date += Essentials.numberWithZero(ca.get(Calendar.DAY_OF_MONTH));
+
+            name = (TextView) findViewById(R.id.ticket_mobile_item_duedate);
+            name.setText(date);
+        } catch (ServerClient.ServerErrorException ex) {
+            Log.e("error!",ex.msg);
+        }
+}
+}
