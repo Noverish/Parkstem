12ef0b7df97811fa79c01707d75db2cf9a7ef6b4
Noverish <embrapers263@gmail.com>
2016-07-14 19:42:23 +0900

    회원 탈퇴 구현

--- app/src/main/java/com/trams/parkstem/activity/SettingActivity.java
+++ app/src/main/java/com/trams/parkstem/activity/SettingActivity.java
@@ -1,15 +1,20 @@
 package com.trams.parkstem.activity;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.trams.parkstem.R;
 import com.trams.parkstem.base_activity.BaseBackSearchActivity;
 import com.trams.parkstem.others.Essentials;
+import com.trams.parkstem.server.ServerClient;
 
 /**
  * Created by Noverish on 2016-07-13.
@@ -44,6 +49,22 @@ public class SettingActivity extends BaseBackSearchActivity {
                 Essentials.makePopup(context, title, content);
             }
         });
+
+        LinearLayout logoutButton = (LinearLayout) findViewById(R.id.activity_setting_logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+        LinearLayout signOutButton = (LinearLayout)findViewById(R.id.activity_setting_sign_out);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
     }
 
     private void changePushStatus() {
@@ -56,5 +77,33 @@ public class SettingActivity extends BaseBackSearchActivity {
             pushButton.removeAllViews();
             getLayoutInflater().inflate(R.layout.push_button_off, pushButton);
         }
+    }
+
+    private void logout() {
+
+    }
+
+    private void signOut() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle(getString(R.string.popup_sign_out_title))
+                .setMessage(getString(R.string.popup_sign_out_content))
+                .setPositiveButton("탈퇴", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            ServerClient.getInstance().memberDelete();
+                            Toast.makeText(SettingActivity.this, "회원탈퇴에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } catch (ServerClient.ServerErrorException error) {
+                            error.printStackTrace();
+                            Toast.makeText(SettingActivity.this, "회원탈퇴에 실패하였습니다. - " + error.msg,Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton("취소", null)
+                .show();
     }
 }
