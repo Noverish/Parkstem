98a449525cf1ce15596a8005d31e5f4daa7f7fe1
vkfwlsdhs <dbwogy1@naver.com>
2016-07-17 21:14:28 +0900

    ...오류...뜨네열...

--- app/src/main/java/com/trams/parkstem/activity/TicketMobileListActivity.java
+++ app/src/main/java/com/trams/parkstem/activity/TicketMobileListActivity.java
@@ -8,11 +8,13 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.trams.parkstem.R;
 import com.trams.parkstem.base_activity.BaseBackSearchActivity;
 import com.trams.parkstem.server.ServerClient;
 import com.trams.parkstem.view.HistoryPaymentView;
+import com.trams.parkstem.view.LongTicketMobileView;
 import com.trams.parkstem.view.TicketMobileView;
 
 /**
@@ -29,14 +31,25 @@ public class TicketMobileListActivity extends BaseBackSearchActivity {
         LinearLayout content = (LinearLayout) findViewById(R.id.activity_ticket_mobile_list_layout);
 
         ServerClient.TicketLists list;
+        ServerClient.LongTicketLists longlist;
 
         try {
             list = ServerClient.getInstance().listOfTicket();
-            int count=0;
+
             for(ServerClient.Ticket ticket: list.data){
                 TicketMobileView ticketMobileView = new TicketMobileView(this, ticket);
-
+                TextView buttonname = (TextView)ticketMobileView.findViewById(R.id.ticket_mobile_item_button_name);
+                buttonname.setText("사용가능");
                 content.addView(ticketMobileView);
+            }
+
+            longlist = ServerClient.getInstance().listOfLongTicket();
+
+            for(ServerClient.Ticket ticket: longlist.data){
+                LongTicketMobileView longTicketMobileView = new LongTicketMobileView(this, ticket);
+                TextView buttonname = (TextView)longTicketMobileView.findViewById(R.id.long_ticket_mobile_item_button_name);
+                buttonname.setText("사용가능");
+                content.addView(longTicketMobileView);
             }
         } catch (ServerClient.ServerErrorException ex) {
             Log.e("error!",ex.msg);
--- app/src/main/java/com/trams/parkstem/view/LongTicketMobileView.java
+++ app/src/main/java/com/trams/parkstem/view/LongTicketMobileView.java
@@ -16,24 +16,24 @@ import java.util.Calendar;
 /**
  * Created by JaeHyo on 2016-07-13.
  */
-public class TicketMobileView extends LinearLayout {
-    public TicketMobileView(Context context, ServerClient.Ticket ticket) {
+public class LongTicketMobileView extends LinearLayout {
+    public LongTicketMobileView(Context context, ServerClient.Ticket ticket) {
         super(context);
 
         LayoutInflater inflater = LayoutInflater.from(context);
 
         try{
-            inflater.inflate(R.layout.ticket_mobile_item, this);
+            inflater.inflate(R.layout.long_ticket_mobile_item, this);
 
             ServerClient.ParkInfo parkInfo = ServerClient.getInstance().parkInfo(ticket.local_id);
 
-            TextView name = (TextView) findViewById(R.id.ticket_mobile_item_name);
+            TextView name = (TextView) findViewById(R.id.long_ticket_mobile_item_name);
             name.setText(ticket.ticket_name);
 
-            name = (TextView) findViewById(R.id.ticket_mobile_item_place);
+            name = (TextView) findViewById(R.id.long_ticket_mobile_item_place);
             name.setText(parkInfo.local_content);
 
-            name = (TextView) findViewById(R.id.ticket_mobile_item_place_2);
+            name = (TextView) findViewById(R.id.long_ticket_mobile_item_place_2);
             name.setText(parkInfo.local_address);
 
             Calendar ca = Calendar.getInstance();
@@ -43,26 +43,25 @@ public class TicketMobileView extends LinearLayout {
 
             date += Essentials.numberWithZero(ca.get(Calendar.DAY_OF_MONTH));
 
-            name = (TextView) findViewById(R.id.ticket_mobile_item_date);
+            name = (TextView) findViewById(R.id.long_ticket_mobile_item_start_date);
             name.setText(date);
 
-            name = (TextView) findViewById(R.id.ticket_mobile_item_before_price);
-            name.setText("1시간 " + (char)0xffe6 + Essentials.numberWithComma(ticket.original_price) );
+            name = (TextView) findViewById(R.id.long_ticket_mobile_item_before_price);
+            name.setText("1달 " + (char)0xffe6 + Essentials.numberWithComma(ticket.original_price) );
             name.setPaintFlags(name.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
 
-            name = (TextView) findViewById(R.id.ticket_mobile_item_after_price);
-            name.setText("1시간 " + (char)0xffe6 + Essentials.numberWithComma(ticket.price) );
+            name = (TextView) findViewById(R.id.long_ticket_mobile_item_after_price);
+            name.setText("1달 " + (char)0xffe6 + Essentials.numberWithComma(ticket.price) );
 
-            ca.add(Calendar.DAY_OF_MONTH, 99
-            );
+            ca.add(Calendar.DAY_OF_MONTH, 30);
 
-            date = "유효기간:" + date + "-" + ca.get(Calendar.YEAR) + ".";
+            date = "- " + ca.get(Calendar.YEAR) + ".";
 
             date += Essentials.numberWithZero(ca.get(Calendar.MONTH) + 1) + ".";
 
             date += Essentials.numberWithZero(ca.get(Calendar.DAY_OF_MONTH));
 
-            name = (TextView) findViewById(R.id.ticket_mobile_item_duedate);
+            name = (TextView) findViewById(R.id.long_ticket_mobile_item_end_date);
             name.setText(date);
         } catch (ServerClient.ServerErrorException ex) {
             Log.e("error!",ex.msg);
