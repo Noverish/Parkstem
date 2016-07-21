592737d7629095a413a906ce3a69291cb6fdb953
LINDBURG <monc2170@gmail.com>
2016-07-21 19:30:11 +0900

    clasue 완성?

---
+++ app/src/main/java/com/trams/parkstem/clause/Clause3.java
@@ -0,0 +1,30 @@
+package com.trams.parkstem.clause;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+import com.trams.parkstem.server.ServerClient;
+
+/**
+ * Created by monc2 on 2016-07-21.
+ */
+public class Clause3 extends AppCompatActivity{
+    WebView webview = new WebView(this);
+    ServerClient serverClient = new ServerClient();
+    String clause;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(webview);
+
+        try {
+            setContentView(webview);
+            clause = serverClient.clause("3");
+            webview.loadData(clause, "text/html; charset=UTF-8", null);
+        } catch (ServerClient.ServerErrorException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
