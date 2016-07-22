0345b637e061bcb0cfef2f05440e3021d6083ee3
LINDBURG <monc2170@gmail.com>
2016-07-22 21:51:24 +0900

    모바일 인증 간단하게(원래있던 모바일화면에 웹뷰를 띄우지 않고 새로운 창을 하나 생성하였으며 데이터를 입력하지 않고 확인을 눌러도 모바일 인증화면으로 넘어감)

--- app/src/main/java/com/trams/parkstem/certification/CardRegister.java
+++ app/src/main/java/com/trams/parkstem/certification/CardRegister.java
@@ -27,57 +27,21 @@ public class CardRegister extends AppCompatActivity {
         webview.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                if (url.contains("mobile.php")) {
+                if (url.contains("card_reg.php")) {
                     Toast.makeText(CardRegister.this, "success", Toast.LENGTH_LONG).show();
                     finish();
                 }
-//                if (Uri.parse(url).getHost().equals("app.parkstem.com/api/kmcis_mobile.php")) {
-//                    try{
-//                        URL aURL = new URL(url);
-//                        URLConnection conn = aURL.openConnection();
-//                        conn.connect();
-//
-//                        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//
-//                        String line;
-//                        jsonStr = "";
-//
-//                        while ((line = reader.readLine()) != null) {
-//                            jsonStr += line + "\n";
-//                        }
-//                        reader.close();
-//
-//                        result = new JSONObject(jsonStr);
-//
-//                        Toast.makeText(mobile_cert.this, "success", Toast.LENGTH_LONG).show();
-//
-//                    } catch(MalformedURLException ex){
-//                        ex.printStackTrace();
-//                    } catch(IOException ex){
-//                        ex.printStackTrace();
-//                    } catch( JSONException ex){
-//                        ex.printStackTrace();
-//                    }
-//                }
             }
-
-//            @Override
-//            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request){
-//                if (url.contains(".js")) {
-//                    return getWebResourceResponseFromString();
-//                } else {
-//                    return super.shouldInterceptRequest(view, url);
-//                }
-//            }
-//            private WebResourceResponse getWebResourceResponseFromString() {
-//                return getUtf8EncodedWebResourceResponse(new StringBufferInputStream("alert('!NO!')"));
-//            }
-//            private WebResourceResponse getUtf8EncodedWebResourceResponse(InputStream data) {
-//                return new WebResourceResponse("text/css", "UTF-8", data);
-//            }
         });
 
 
-        webview.loadUrl("http://app.parkstem.com/api/kmcis_start.php?uniqueID=" + serverClient.uniqueID);
+        String url = "https://inilite.inicis.com/inibill/inibill_card.jsp";
+        String data = "returnurl=http://app.parkstem.com/api/card_reg.php&mid=hotelvey11&goodname=certification&price=1&type=1&orderid=AAAAA&notice=good&timestamp=20160427171717&period=0&hashdata=0c4b70d28e3dfbdf6561d3aff631f8355a3991c965223bd88285a8d9f8c0e935&p_not i=card_name";
+        try {
+            byte[] postdata = data.getBytes("UTF-8");
+            webview.postUrl(url, postdata);
+        } catch (Exception ex) {
+
+        }
     }
 }
