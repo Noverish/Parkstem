0212238a976f9de7f751dd85dc49884b47a75074
Noverish <embrapers263@gmail.com>
2016-07-23 14:14:41 +0900

    시작할 때 필요한 작업 처리를 스플레시 이미지를 띄울 때 처리 하도록 함.

--- app/src/main/java/com/trams/parkstem/activity/LoginActivity.java
+++ app/src/main/java/com/trams/parkstem/activity/LoginActivity.java
@@ -1,26 +1,17 @@
 package com.trams.parkstem.activity;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.FacebookSdk;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.trams.parkstem.R;
 import com.trams.parkstem.base_activity.BaseNavigationActivity;
-import com.trams.parkstem.gcm.QuickstartPreferences;
-import com.trams.parkstem.gcm.RegistrationIntentService;
 import com.trams.parkstem.others.FacebookLoginClient;
 import com.trams.parkstem.others.KakaoLoginClient;
 import com.trams.parkstem.others.NaverLoginClient;
@@ -37,7 +28,7 @@ public class LoginActivity extends AppCompatActivity implements OnLoginSuccessLi
     private KakaoLoginClient kakaoLoginClient;
     private LoginDatabase loginDatabase;
 
-    private String gcmDeviceToken = "token";
+    private String gcmDeviceToken = "login_activity_token_error";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,23 +36,9 @@ public class LoginActivity extends AppCompatActivity implements OnLoginSuccessLi
         FacebookSdk.sdkInitialize(getApplicationContext()); // xml을 불러 오기 전에 해야 됨
         setContentView(R.layout.activity_login);
 
-        registBroadcastReceiver();
-        getInstanceIdToken();
+        gcmDeviceToken = getIntent().getStringExtra("token");
 
         loginDatabase = LoginDatabase.getInstance(this);
-        if(!loginDatabase.isDatabaseClear()) {
-            try {
-                ServerClient.getInstance().login(loginDatabase.getGubun(), loginDatabase.getId(), loginDatabase.getPw(), gcmDeviceToken);
-
-                Toast.makeText(this, "자동 로그인 되었습니다!", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(this, HomeActivity.class);
-                startActivity(intent);
-                finish();
-            } catch (ServerClient.ServerErrorException ex) {
-                Toast.makeText(this, "자동 로그인에 실패 했습니다 - " + ex.msg, Toast.LENGTH_SHORT).show();
-            }
-        }
 
         naverLoginClient = NaverLoginClient.getInstance(this);
         naverLoginClient.setOnLoginSuccessListener(this);
@@ -142,92 +119,5 @@ public class LoginActivity extends AppCompatActivity implements OnLoginSuccessLi
         } catch (ServerClient.ServerErrorException ex) {
             Toast.makeText(LoginActivity.this, "로그인에 실패 했습니다 - " + ex.msg, Toast.LENGTH_SHORT).show();
         }
-    }
-
-
-
-
-    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    private final String TAG = getClass().getSimpleName();
-
-    private BroadcastReceiver mRegistrationBroadcastReceiver;
-
-    /**
-     * Instance ID를 이용하여 디바이스 토큰을 가져오는 RegistrationIntentService를 실행한다.
-     */
-    public void getInstanceIdToken() {
-        if (checkPlayServices()) {
-            // Start IntentService to register this application with GCM.
-            Intent intent = new Intent(this, RegistrationIntentService.class);
-            startService(intent);
-        }
-    }
-
-    /**
-     * LocalBroadcast 리시버를 정의한다. 토큰을 획득하기 위한 READY, GENERATING, COMPLETE 액션에 따라 UI에 변화를 준다.
-     */
-    public void registBroadcastReceiver(){
-        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-
-
-                if(action.equals(QuickstartPreferences.REGISTRATION_READY)){
-                    // 액션이 READY일 경우
-                    Log.e("GCM","READY");
-                } else if(action.equals(QuickstartPreferences.REGISTRATION_GENERATING)){
-                    // 액션이 GENERATING일 경우
-                    Log.e("GCM","GENERATING");
-                } else if(action.equals(QuickstartPreferences.REGISTRATION_COMPLETE)){
-                    // 액션이 COMPLETE일 경우
-                    String token = intent.getStringExtra("token");
-                    Log.e("GCM","COMPLETE - " + token);
-                    gcmDeviceToken = token;
-                }
-            }
-        };
-    }
-
-    /**
-     * 앱이 실행되어 화면에 나타날때 LocalBoardcastManager에 액션을 정의하여 등록한다.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_READY));
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_GENERATING));
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
-
-    }
-
-    /**
-     * 앱이 화면에서 사라지면 등록된 LocalBoardcast를 모두 삭제한다.
-     */
-    @Override
-    protected void onPause() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
-        super.onPause();
-    }
-
-    /**
-     * Google Play Service를 사용할 수 있는 환경이지를 체크한다.
-     */
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(TAG, "This device is not supported.");
-                finish();
-            }
-            return false;
-        }
-        return true;
     }
 }
--- app/src/main/java/com/trams/parkstem/activity/SplashActivity.java
+++ app/src/main/java/com/trams/parkstem/activity/SplashActivity.java
@@ -1,36 +1,182 @@
 package com.trams.parkstem.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.trams.parkstem.R;
+import com.trams.parkstem.gcm.QuickstartPreferences;
+import com.trams.parkstem.gcm.RegistrationIntentService;
+import com.trams.parkstem.server.LoginDatabase;
+import com.trams.parkstem.server.ServerClient;
 
 /**
  * Created by Noverish on 2016-07-04.
  */
 public class SplashActivity extends AppCompatActivity{
+    private String gcmDeviceToken = "splash_activity_token_error";
+    private boolean backgroundProcessDone = false;
+    private boolean sleepDone = false;
+    private boolean goToLoginActivity = true;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Thread thread = new Thread(new Runnable() {
+        Thread processThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(1500);
+                    registBroadcastReceiver();
+                    getInstanceIdToken();
 
-                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                    SplashActivity.this.startActivity(intent);
-                    finish();
+                    LoginDatabase loginDatabase = LoginDatabase.getInstance(SplashActivity.this);
+                    if(!loginDatabase.isDatabaseClear()) {
+                        try {
+                            ServerClient.getInstance().login(loginDatabase.getGubun(), loginDatabase.getId(), loginDatabase.getPw(), gcmDeviceToken);
+
+                            Toast.makeText(SplashActivity.this, "자동 로그인 되었습니다!", Toast.LENGTH_SHORT).show();
+
+                            goToLoginActivity = false;
+                        } catch (ServerClient.ServerErrorException ex) {
+                            Toast.makeText(SplashActivity.this, "자동 로그인에 실패 했습니다 - " + ex.msg, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    backgroundProcessDone = true;
+
+                    if(sleepDone) {
+                        changeActivity();
+                    }
                 } catch (Exception ex) {
 
                 }
             }
         });
+        processThread.start();
 
-        thread.start();
+        Thread sleepThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException ex) {
+
+                }
+
+                sleepDone = true;
+                if(backgroundProcessDone) {
+                    changeActivity();
+                }
+            }
+        });
+        sleepThread.start();
+    }
+
+    private void changeActivity() {
+        if (goToLoginActivity) {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            intent.putExtra("token",gcmDeviceToken);
+            SplashActivity.this.startActivity(intent);
+            finish();
+        } else {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private final String TAG = getClass().getSimpleName();
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+
+    /**
+     * Instance ID를 이용하여 디바이스 토큰을 가져오는 RegistrationIntentService를 실행한다.
+     */
+    public void getInstanceIdToken() {
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        } else {
+            Toast.makeText(this, "구글 플레이 서비스를 이용할 수 없습니다.\n푸쉬알림을 못 받을 수도 있습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * LocalBroadcast 리시버를 정의한다. 토큰을 획득하기 위한 READY, GENERATING, COMPLETE 액션에 따라 UI에 변화를 준다.
+     */
+    public void registBroadcastReceiver(){
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+                if(action.equals(QuickstartPreferences.REGISTRATION_READY)){
+                    // 액션이 READY일 경우
+                    Log.e("GCM","READY");
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_GENERATING)){
+                    // 액션이 GENERATING일 경우
+                    Log.e("GCM","GENERATING");
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_COMPLETE)){
+                    // 액션이 COMPLETE일 경우
+                    String token = intent.getStringExtra("token");
+                    Log.e("GCM","COMPLETE - " + token);
+                    gcmDeviceToken = token;
+                }
+            }
+        };
+    }
+
+    /**
+     * 앱이 실행되어 화면에 나타날때 LocalBoardcastManager에 액션을 정의하여 등록한다.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_READY));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_GENERATING));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+
+    }
+
+    /**
+     * 앱이 화면에서 사라지면 등록된 LocalBoardcast를 모두 삭제한다.
+     */
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+
+    /**
+     * Google Play Service를 사용할 수 있는 환경이지를 체크한다.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
