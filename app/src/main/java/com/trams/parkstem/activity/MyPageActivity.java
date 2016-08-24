e8d66615ce45633bec42c3bf877aac07f599eb32
Noverish <embrapers263@gmail.com>
2016-08-24 19:56:22 +0900

    마이페이지에서 버튼 누르는 모션 추가

--- app/src/main/java/com/trams/parkstem/activity/MyPageActivity.java
+++ app/src/main/java/com/trams/parkstem/activity/MyPageActivity.java
@@ -1,10 +1,14 @@
 package com.trams.parkstem.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.trams.parkstem.R;
@@ -15,12 +19,34 @@ import com.trams.parkstem.server.ServerClient;
 /**
  * Created by Noverish on 2016-08-24.
  */
-public class MyPageActivity extends BaseBackSearchActivity {
+public class MyPageActivity extends BaseBackSearchActivity implements View.OnTouchListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
         setTitle("마이페이지");
+
+        RelativeLayout changeEmail = (RelativeLayout) findViewById(R.id.activity_my_page_change_email);
+        if(changeEmail != null) {
+            changeEmail.setOnTouchListener(this);
+            changeEmail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        RelativeLayout changePassword = (RelativeLayout) findViewById(R.id.activity_my_page_change_password);
+        if(changePassword != null) {
+            changePassword.setOnTouchListener(this);
+            changePassword.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
     }
 
     @Override
@@ -68,5 +94,29 @@ public class MyPageActivity extends BaseBackSearchActivity {
         if(phone != null) {
             phone.setText(ServerClient.getInstance().getUserMobile());
         }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        try {
+            Context context = MyPageActivity.this;
+            RelativeLayout background = (RelativeLayout) v;
+            TextView text = (TextView) ((RelativeLayout) v).getChildAt(0);
+
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                background.setBackground(ContextCompat.getDrawable(context, R.drawable.btn_1));
+                text.setTextColor(ContextCompat.getColor(context, R.color.WHITE));
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                background.setBackground(ContextCompat.getDrawable(context, R.drawable.box_edit));
+                text.setTextColor(ContextCompat.getColor(context, R.color.gray_for_text));
+            } else {
+                if(event.getAction() != MotionEvent.ACTION_MOVE)
+                    Log.e("MotionEvent", event.getAction() + "");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Log.e("ERROR","Unknown ERROR");
+        }
+        return false;
     }
 }
