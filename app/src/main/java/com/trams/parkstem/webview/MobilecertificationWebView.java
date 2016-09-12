08ac495bf5bde9802b18810b035d0b7edd4724f6
Noverish <embrapers263@gmail.com>
2016-09-12 11:19:01 +0900

    휴대폰 인증 후 server api로 인증이 된 것을 확인하는게 아니라 webview에 뜬 내용을 보고 인증이 된 것을 확인함

--- app/src/main/java/com/trams/parkstem/webview/MobilecertificationWebView.java
+++ app/src/main/java/com/trams/parkstem/webview/MobilecertificationWebView.java
@@ -1,8 +1,9 @@
 package com.trams.parkstem.webview;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -11,6 +12,9 @@ import android.widget.Toast;
 import com.trams.parkstem.R;
 import com.trams.parkstem.base_activity.BaseBackSearchActivity;
 import com.trams.parkstem.server.ServerClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by monc2 on 2016-07-22.
@@ -39,23 +43,41 @@ public class MobilecertificationWebView extends BaseBackSearchActivity {
         backButton.setVisibility(View.GONE);
 
         webview.getSettings().setJavaScriptEnabled(true);
+        webview.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
         webview.setWebViewClient(new WebViewClient() {
             @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            public void onPageFinished(WebView view, String url) {
+                Log.i("url",url);
                 if (url.contains("kmcis_mobile.php")) {
-                    try {
-                        if(ServerClient.getInstance().memberInfo().certification)
-                            Toast.makeText(MobilecertificationWebView.this, "휴대폰 인증에 성공했습니다", Toast.LENGTH_SHORT).show();
-                        else
-                            Toast.makeText(MobilecertificationWebView.this, "휴대폰 인증에 실패했습니다", Toast.LENGTH_SHORT).show();
-                    } catch (ServerClient.ServerErrorException ex) {
-                        Toast.makeText(MobilecertificationWebView.this, "휴대폰 인증에 실패했습니다 - " + ex.msg, Toast.LENGTH_SHORT).show();
-                    }
-                    finish();
+                    view.loadUrl("javascript:window.HTMLOUT.processHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
                 }
             }
         });
 
         webview.loadUrl("http://app.parkstem.com/api/kmcis_start.php?uniqueID=" + serverClient.getUniqueID());
+    }
+
+
+    /* An instance of this class will be registered as a JavaScript interface */
+    private class MyJavaScriptInterface
+    {
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        public void processHTML(String html) {
+            html = html.replaceAll("<[^>]*>", "");
+
+            try {
+                JSONObject result = new JSONObject(html);
+                if(result.getInt("res") == 1) {
+                    Toast.makeText(MobilecertificationWebView.this, "휴대폰 인증에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MobilecertificationWebView.this, "휴대폰 인증에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException ex) {
+                Toast.makeText(MobilecertificationWebView.this, "휴대폰 인증하는데 오류가 발생했습니다. - JSON ERROR", Toast.LENGTH_SHORT).show();
+            } finally {
+                finish();
+            }
+        }
     }
 }
